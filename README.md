a478f119426af313208de4f65e2dd75a3e42a088
Dennis Wilkins <dennis@deliciousmorsel.com>
2013-05-09 10:24:33 -0600

    Init setup

---
+++ README.md
@@ -0,0 +1,28 @@
+ThumbsUp.js
+=========
+
+![ThumbsUp.js example image](https://github.com/inlineblock/ThumbsUp/raw/master/example.png)
+
+
+## Easy way to run/test the index.html
+```bash
+$ cd ThumbsUp
+$ python -m SimpleHTTPServer
+```
+
+## Embedding FontAwesome
+```css
+@font-face {
+  font-family: 'FontAwesome';
+  src: url('http://fortawesome.github.io/Font-Awesome/assets/font/fontawesome-webfont.eot?v=3.0.1');
+  src: url('http://fortawesome.github.io/Font-Awesome/assets/font/fontawesome-webfont.eot?#iefix&v=3.0.1') format('embedded-opentype'),
+    url('http://fortawesome.github.io/Font-Awesome/assets/font/fontawesome-webfont.woff?v=3.0.1') format('woff'),
+    url('http://fortawesome.github.io/Font-Awesome/assets/font/fontawesome-webfont.ttf?v=3.0.1') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+```
+
+LICENSE
+=======
+[MIT License](http://opensource.org/licenses/MIT)
---
+++ ThumbsUp.js
@@ -0,0 +1,36 @@
+var ThumbsUp = function (element , o) {
+  o = o || {};
+  var size = 13,
+    $body = $(document.body),
+    $el = $('<div class="thumbsup">' + (o.character || '&#xf087;') + '</div>'),
+    $anchor = $(element),
+    offset = $anchor.offset(),
+    windowHeight = $(window).height(),
+    windowWidth = $(window).width(),
+    newFontSize = Math.floor(Math.min(windowHeight , windowWidth) * 1.2),
+    color = o.color || 'black',
+    easing = o.easing || 'swing',
+    speed = o.speed || 750,
+    newTop = (windowHeight/2) - (newFontSize/2),
+    newLeft = (windowWidth/2) - ((newFontSize)/2);
+
+    $body.append($el);
+    $el.css({
+      fontFamily: 'FontAwesome',
+      position: 'fixed',
+      top: offset.top - $body.scrollTop(),
+      left: offset.left - $body.scrollLeft(),
+      fontSize: size,
+      opacity: 1,
+      width: newFontSize,
+      color: color,
+    });
+    $el.animate({
+      fontSize: newFontSize,
+      opacity: 0,
+      left: newLeft < 0 ? 0 : newLeft,
+      top: newTop < 0 ? 0 : newTop
+    } , speed , easing , function () {
+      $(this).remove()
+    })
+}
